a770ae5e03f738b3e7ee2f563356262dfc966ba0
kellysmithhh <119341531+kellysmithhh@users.noreply.github.com>
2025-09-25 16:23:19 +1000

    Add to Cart updates with correct quantities

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,17 +25,17 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-                <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                <select class="js-quantity-selector-${product.id}">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
                 </select>
             </div>
 
@@ -61,7 +61,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
         let matchingItem;
-
+        let totalQuantitySelected = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+       
         cart.forEach((item) => {
             if (productId === item.productId) {
                 matchingItem = item;
@@ -69,11 +70,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         })
 
         if (matchingItem) {
-            matchingItem.quantity += 1;
+            matchingItem.quantity += totalQuantitySelected;
         } else {
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: totalQuantitySelected
             });
         }
        
@@ -83,5 +84,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         });
         
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        console.log(cart);
     });
 });
