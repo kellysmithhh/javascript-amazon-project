7cfa596444afbfe90c533754124d4dd777fe9dee
kellysmithhh <119341531+kellysmithhh@users.noreply.github.com>
2025-09-26 16:16:13 +1000

    make delivery iotuo

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -89,7 +89,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html +=
-        `<div class="delivery-option">
+        `<div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
             <input type="radio" 
             ${isChecked ? 'checked' : ''}
             class="delivery-option-input"
@@ -154,3 +156,11 @@ document.querySelectorAll('.save-quantity-link').forEach((link) => {
         document.querySelector('.js-return-to-home-link').innerHTML = getCartQuantity();
     });
 });
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+        const productId = element.dataset.productId;
+        const deliveryOptionId = element.dataset.deliveryOptionId;
+        updateDeliveryOption(productId, deliveryOptionId);
+    });
+})
